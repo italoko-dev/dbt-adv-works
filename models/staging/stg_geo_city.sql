37e6dbe0796965f6fc5266962905ce5e8cfc7add
Italo Piovan <italo.rocha@indicium.tech>
2024-10-18 18:01:33 +0000

    add state_province_fk

--- models/staging/stg_geo_city.sql
+++ models/staging/stg_geo_city.sql
@@ -2,6 +2,8 @@ with
     geo_address as (
         select
             cast(addressid as int) as address_pk
+            , cast(stateprovinceid as int) as state_province_fk
+
             , cast(city as string) as address_city_name
         from {{ source('adv_works', 'address') }}        
     )
