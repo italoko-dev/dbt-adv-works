a4dbf8a2d0316b8438e26cef03c6c428de3303eb
Italo Piovan <italo.rocha@indicium.tech>
2024-10-23 20:02:02 +0000

    added metrics in int_sales intermediate model

--- models/intermediate/int_sales.sql
+++ models/intermediate/int_sales.sql
@@ -8,14 +8,16 @@ with
             , sales_order_territory_fk
             , sales_order_cred_card_fk
             , sales_order_address_id_fk
-                        
             , sales_order_date
-            , sales_order_status
-            
-            , sales_order_subtotal
+            , case 
+                when sales_order_status = 1 then 'In Process'
+                when sales_order_status = 2 then 'Approved'
+                when sales_order_status = 3 then 'Backordered'
+                when sales_order_status = 4 then 'Rejected'
+                when sales_order_status = 5 then 'Shipped'
+                when sales_order_status = 6 then 'Cancelled'
+            end as sales_order_status
             , sales_order_freight
-            , sales_order_taxamt
-            , sales_order_total_due
         from {{ ref('stg_sales_order_header') }}
     )
 
@@ -36,8 +38,6 @@ with
         select
             sales_header.sales_order_date
             , sales_header.sales_order_header_pk
-            -- sales_detail.sales_order_header_fk
-            -- , sales_detail.sales_order_detail_pk
             , sales_header.sales_order_date_fk
             , sales_detail.sales_order_product_fk
             , sales_detail.sales_order_qtd
@@ -52,12 +52,37 @@ with
             , sales_header.sales_order_address_id_fk
             
             , sales_header.sales_order_status
-            , sales_header.sales_order_subtotal
-            , sales_header.sales_order_taxamt
             , sales_header.sales_order_freight
-            , sales_header.sales_order_total_due
         from sales_detail left join sales_header on 
             sales_detail.sales_order_header_fk = sales_header.sales_order_header_pk
     )
+
+    , metrics as (
+        select
+            sales_order_date
+            , sales_order_header_pk
+            , sales_order_date_fk
+            , sales_order_product_fk
+            , sales_order_special_offer_fk
+            , sales_order_customer_fk
+            , sales_order_sales_person_fk
+            , sales_order_territory_fk
+            , sales_order_cred_card_fk
+            , sales_order_address_id_fk
+            , sales_order_qtd
+            , sales_order_prod_unit_price
+            , sales_order_prod_discount
+            , sales_order_freight / (count(sales_order_header_pk) over (
+                    partition by sales_order_header_pk
+                )
+            ) as sales_order_freight_by_product
+            , sales_order_qtd 
+                * sales_order_prod_unit_price
+                * (1 - sales_order_prod_discount)
+            as sales_order_subtotal -- liquido
+            , sales_order_qtd * sales_order_prod_unit_price as sales_order_total_due -- bruto
+            , sales_order_status
+        from detailed_sale
+    )
 select  *
-from detailed_sale
+from metrics
